fec20b3ec19dbf6c31b2b505f9ae55a20cad1587
sajid-sec <sajidali20071901@gmail.com>
2026-09-14 15:25:49 +0530

    Refactor punctuation removal in Python program

--- Write a Python program to remove all punctuation characters from a string using string methods or the string module.py
+++ Write a Python program to remove all punctuation characters from a string using string methods or the string module.py
@@ -1,9 +1,9 @@
 # 5. Write a Python program to remove all punctuation characters from a string using string methods or the string module. 
 
-import string
-s = input("Enter a string: ")
+import string     #importing mstring module
+s = input("Enter a string: ") #enter a string
 result = ""
 for i in s:
-    if i not in string.punctuation:
-        result += i
+    if i not in string.punctuation:       #checking punc
+        result += i       #adding
 print("String without Punctuation:", result)
