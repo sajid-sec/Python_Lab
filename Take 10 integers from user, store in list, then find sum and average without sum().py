37ee63ff4dfb1ad3f9ea320d7cdc1f170f5e4112
sajid-sec <sajidali20071901@gmail.com>
2026-09-14 15:20:36 +0530

    Refactor sum and average calculation logic

--- Take 10 integers from user, store in list, then find sum and average without sum().py
+++ Take 10 integers from user, store in list, then find sum and average without sum().py
@@ -1,11 +1,11 @@
 # 2. Take 10 integers from user, store in list, then find sum and average without sum()
 
 numbers = []
-for i in range(10):
-	numbers.append(int(input("Enter integer: ")))
-total = 0
+for i in range(10): #looping
+	numbers.append(int(input("Enter integer: ")))     #appending
+total = 0 #for sum
 for j in numbers:
-	total += j
-average = total / len(numbers)
+	total += j        #adding
+average = total / len(numbers)            #avg
 print("Sum:", total)
 print("Average:", average)
