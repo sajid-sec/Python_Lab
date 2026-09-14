0f8ab160e7db67cf176e9979f00b74ca4850133b
sajid-sec <sajidali20071901@gmail.com>
2026-09-14 15:08:44 +0530

    Add fruit list manipulation in p1.py

---
+++ p1.py
@@ -0,0 +1,5 @@
+# 1. Create a list of 5 fruits, print 2nd and 4th items, replace last with "Mango", and print updated list.
+fruits = ["Apple", "Banana", "Orange", "Grapes", "Pineapple"]
+print(fruits[1], fruits[3])
+fruits[-1] = "Mango"
+print(fruits)
