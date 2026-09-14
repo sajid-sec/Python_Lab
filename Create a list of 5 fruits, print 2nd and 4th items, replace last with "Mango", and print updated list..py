4c6921dac705bd0118b10d062351f19fb91e2d46
sajid-sec <sajidali20071901@gmail.com>
2026-09-14 15:18:39 +0530

    Modify fruit list and print specific items

--- Create a list of 5 fruits, print 2nd and 4th items, replace last with "Mango", and print updated list..py
+++ Create a list of 5 fruits, print 2nd and 4th items, replace last with "Mango", and print updated list..py
@@ -1,5 +1,5 @@
 # 1. Create a list of 5 fruits, print 2nd and 4th items, replace last with "Mango", and print updated list.
-fruits = ["Apple", "Banana", "Orange", "Grapes", "Pineapple"]
-print(fruits[1], fruits[3])
-fruits[-1] = "Mango"
+fruits = ["Apple", "Banana", "Orange", "Grapes", "Pineapple"]   #list
+print(fruits[1], fruits[3])       #print
+fruits[-1] = "Mango"      #replacing last word
 print(fruits)
